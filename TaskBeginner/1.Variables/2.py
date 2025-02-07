1f13f2be55d2bce8bb4a1f31b6e238727f77abb5
Prajwal Vyavahare <122688935+2002Praju@users.noreply.github.com>
2025-02-07 09:54:44 +0000

    OVERHEAD

---
+++ TaskBeginner/1.Variables/2.py
@@ -0,0 +1,6 @@
+
+#2. Create a variable called for and assign it a value 4. See what
+#happens and find out the reason behind the behavior that you
+#see. 
+for = 4
+""" Syntax Error -because for is keyword which cannot be used as a variable name"""
---
+++ TaskBeginner/1.Variables/3.py
@@ -0,0 +1,9 @@
+
+#3. Store the principal amount, rate of interest, and time in
+#different variables and then calculate the Simple Interest for 3
+#years. Formula: Simple Interest = P x R x T / 100
+P = float(input("Enter the principal amount"))
+R = float(input("Enter the rate of interest"))
+T = 18     #Time in months
+SI = (P * R * T)/100
+print(f"The simple interest is {SI}")
